7bc7618d1d687539a0aa83c7bde29fa82cb061f3
Goddness, i'm a fear <149561574+sufiren0@users.noreply.github.com>
2024-06-20 21:14:55 +0700

    Update z4gs-modded.lua

--- z4gs-modded.lua
+++ z4gs-modded.lua
@@ -153,7 +153,7 @@ end
 
 tab2:AddSlider("TP Speed", function(x)
     myData.TeleportSpeed = x
-end, {min = 150, max = 250}):Set(45)
+end, {min = 90, max = 250}):Set(45)
 
 tab2:AddSlider("Distance from NPC", function(x)
     myData.DistanceFromNpc = x * -1
